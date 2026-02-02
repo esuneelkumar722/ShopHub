04be6219f4be76441643cecb060ef01077d00c42
Enturi Suneelkumar <suneel.kumar.enturi@accenture.com>
2026-02-02 12:05:42 +0530

    fix: update copyright year in Footer component to use current year dynamically

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -43,7 +43,7 @@ export const Footer = () => {
         </div>
 
         <div className="border-t border-gray-800 mt-8 pt-8 text-center text-gray-400">
-          <p>&copy; 2024 ShopHub. All rights reserved.</p>
+          <p>&copy; {new Date().getFullYear()} ShopHub. All rights reserved.</p>
         </div>
       </div>
     </footer>
